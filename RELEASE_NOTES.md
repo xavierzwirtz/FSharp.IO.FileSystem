5d411088c5408987d3c9b2c025cd4421b4d244ed
voiceofwisdom <me@xavierzwirtz.com>
2015-11-20 20:35:37 -0600

    Bump version to 1.1.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 1.1.0 - November 20 2015
+* Wrapped everything that can fail with Chessie
+
 #### 1.0.0 - November 19 2015
 * Initial release
--- src/FSharp.IO.FileSystem/AssemblyInfo.fs
+++ src/FSharp.IO.FileSystem/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.IO.FileSystem")>]
 [<assembly: AssemblyProductAttribute("FSharp.IO.FileSystem")>]
 [<assembly: AssemblyDescriptionAttribute("FSharp'y methods of reading and writing to the file system.")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0"
+    let [<Literal>] Version = "1.1.0"
